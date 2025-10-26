bdce558a0a62d3629bcd5004c3ca6cd98319b5e5
mdjdkq <ho8178616@gmail.com>
2025-10-26 14:06:42 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/example/autoengineer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.autoengineer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello from AutoEngineer!");
+        setContentView(tv);
+    }
+}
